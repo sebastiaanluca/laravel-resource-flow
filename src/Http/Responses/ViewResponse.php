3626cef29a82f19cfe7e3718f6b60a5e254f484d
Sebastiaan Luca <hello@sebastiaanluca.com>
2017-08-25 17:27:57 +0200

    Switch ShowsViews trait with a responsable

---
+++ src/Http/Responses/ViewResponse.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace SebastiaanLuca\Flow\Http\Responses;
+
+use Illuminate\Contracts\Support\Responsable;
+
+abstract class ViewResponse implements Responsable
+{
+    /**
+     * @var array
+     */
+    protected $viewData = [];
+
+    /**
+     * Create an HTTP response that represents the object.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function toResponse($request)
+    {
+        return view($this->getView(), $this->viewData);
+    }
+
+    /**
+     * @return string
+     */
+    abstract public function getView() : string;
+
+    /**
+     * @param array|string $key
+     * @param mixed $value
+     *
+     * @return $this
+     */
+    public function addViewData($key, $value = null)
+    {
+        $data = $key;
+
+        if (! is_array($key)) {
+            $data = [$key => $value];
+        }
+
+        $this->viewData = array_merge($this->viewData, $data);
+
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getViewData() : array
+    {
+        return $this->viewData;
+    }
+}
