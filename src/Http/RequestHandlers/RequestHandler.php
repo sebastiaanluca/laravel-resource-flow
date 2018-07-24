6ea3216c4414c907824981dfefc90340a58353ff
Sebastiaan Luca <hello@sebastiaanluca.com>
2018-07-24 14:00:54 +0200

    Extend request handler

--- src/Http/RequestHandlers/RequestHandler.php
+++ src/Http/RequestHandlers/RequestHandler.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace SebastiaanLuca\Flow\Http\RequestHandlers;
 
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller;
 
 class RequestHandler extends Controller
 {
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
     /**
      * Executed when the object itself is called as a method.
      *
@@ -17,10 +22,11 @@ class RequestHandler extends Controller
      */
     public function __invoke()
     {
+        if (method_exists($this, 'before')) {
+            app()->call([$this, 'before'], func_get_args());
+        }
+
         // Use app to make the call so method dependency injection can be used when desired
-        return app()->call(
-            [$this, 'handle'],
-            func_get_args()
-        );
+        return app()->call([$this, 'handle'], func_get_args());
     }
 }
