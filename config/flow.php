c0e0632391c6cc6929fad5f4672791d2ce2db2b8
Sebastiaan Luca <hello@sebastiaanluca.com>
2018-07-24 14:00:30 +0200

    Update config

--- config/flow.php
+++ config/flow.php
@@ -2,16 +2,4 @@
 
 return [
 
-    /*
-     * Eloquent factories should only be registered in local development
-     * environments. Of course you can add your production environment to
-     * this to enable them there too.
-     */
-    'development_environments' => [
-        'local',
-        'dev',
-        'development',
-        'testing',
-    ],
-
 ];
