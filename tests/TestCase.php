38978aea5eefa1bff289fbabc9a69779ae746286
Sebastiaan Luca <hello@sebastiaanluca.com>
2018-07-26 17:15:01 +0200

    Clean up TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,18 +3,9 @@
 namespace SebastiaanLuca\Flow\Tests;
 
 use Orchestra\Testbench\TestCase as BaseTestCase;
-use SebastiaanLuca\Migrations\Providers\UnbreakableMigrationsServiceProvider;
 
 class TestCase extends BaseTestCase
 {
-    /**
-     * @param $app
-     */
-    protected function getEnvironmentSetUp($app)
-    {
-        $app->setBasePath(__DIR__ . '/files');
-    }
-
     /**
      * @param $app
      *
@@ -25,17 +16,5 @@ class TestCase extends BaseTestCase
         return [
             //
         ];
-    }
-
-    /**
-     * Clean up the testing environment before the next test.
-     */
-    protected function tearDown()
-    {
-        parent::tearDown();
-
-        $tempDir = __DIR__ . '/files';
-
-        system('/bin/rm -rf ' . escapeshellarg($tempDir));
     }
 }
